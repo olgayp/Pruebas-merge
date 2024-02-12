3b3667c9cec40f480f87a5b635d40b8895e141a7
docencia <docencia@>
2024-02-12 11:31:49 +0100

    Animal constructor

--- Animal.java
+++ Animal.java
@@ -1,4 +1,9 @@
 public class Animal {
     private String nombre;
     private int edad;
+
+    public Animal(String pNom, int pEdad){
+        this.nombre = pNom;
+        this.edad = pEdad;
+    }
 }
