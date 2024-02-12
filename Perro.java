e6e6e4cfaef5f34e80c360d133d4029ffd992133
docencia <docencia@>
2024-02-12 11:37:05 +0100

    Perro getter + setter

--- Perro.java
+++ Perro.java
@@ -1,6 +1,14 @@
 public class Perro extends Animal{
     private String raza;
 
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
     public Perro(String pNom, int pEdad, String pRaza) {
         super(pNom, pEdad);
         this.raza = pRaza;
